4fc413ec362271ea6340f2e5c5b6c649189397c1
aryadavare19 <153051485+aryadavare19@users.noreply.github.com>
2025-04-29 21:42:52 +0530

    Update db.py

--- CodeCrusaders 113-WomenSafety-SecureHer/db.py
+++ CodeCrusaders 113-WomenSafety-SecureHer/db.py
@@ -3,7 +3,7 @@ import mysql.connector
 # Database connection
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
+        host="localhost",# connecting to local mysql server
         user="root",
         password="root",  # Change if needed
         database="SecureHer"
